ad038813ba6fc5bd5c42d91883f592bbd09cba69
João Lucas Yonea <assasinomortal321321@gmail.com>
2023-10-18 22:02:22 -0300

    inicio da implementacao do projeto

---
+++ ComputacaoGraficaA3/src/KeyBoard.java
@@ -0,0 +1,33 @@
+import com.jogamp.newt.event.KeyEvent;
+import com.jogamp.newt.event.KeyListener;
+/**
+ *
+ * @author Kakugawa
+ */
+public class KeyBoard implements KeyListener{
+    private Cena cena;
+    
+    public KeyBoard(Cena cena){
+        this.cena = cena;
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e) {        
+        System.out.println("Key pressed: " + e.getKeyCode());
+        if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
+            System.exit(0);
+
+        if(e.getKeyChar() == 'a')
+            System.out.println("Pressionou tecla a");
+
+        if(e.getKeyChar() == 'p')
+            cena.localizacao += 0.1f;
+        if(e.getKeyChar() == 'u')
+            cena.localizacao -= 0.1f;
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) { }
+
+}
